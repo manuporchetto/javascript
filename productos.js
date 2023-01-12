188be3f1a47045b9109b88194e343a0e5c9f4ee9
Manuela Porchetto <manuelaporchetto@hotmail.com.ar>
2023-01-12 19:33:03 -0300

    cambios en productos

---
+++ productos.js
@@ -0,0 +1,62 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Buzo",
+        tipo: "buzo",
+        descripcion: "Buzo canguro",
+        precio: 3000,
+        talle: "M",
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Remera",
+        tipo: "remera",
+        descripcion: "Remera estampada",
+        precio: 1500,
+        talle: "S",
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Camisa",
+        tipo: "camisa",
+        descripcion: "Camisa floreada",
+        precio: 200,
+        talle: "M",
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Zapatos",
+        tipo: "zapatos",
+        descripcion: "Zapatos Stiletto negros",
+        precio: 16000,
+        talle: 42,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Lentes",
+        tipo: "lentes",
+        descripcion: "Lentes color rojo",
+        precio: 15000,
+        talle: 4,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Zapatillas",
+        tipo: "zapatillas",
+        descripcion: "Zapatillas color azul",
+        precio: 9000,
+        talle: 38,
+        cantidad:0
+    }
+]
